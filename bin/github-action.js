cb118588724832ab4959a5d861ed8d7227681c38
Wes Morgan <wesmorgan@icloud.com>
2020-01-15 11:07:37 -0700

    Try using top-level async fn wrapper
    Fucking javascript man...

--- bin/github-action.js
+++ bin/github-action.js
@@ -1,22 +1,24 @@
 const core = require('@actions/core');
 const deploy = require('../src');
 
-try {
-  const pinningService = core.getInput('pinning-service');
-  const pinataAPIKey = core.getInput('pinata-api-key');
-  const pinataSecretAPIKey = core.getInput('pinata-secret-api-key');
+(async () => {
+  try {
+    const pinningService = core.getInput('pinning-service');
+    const pinataAPIKey = core.getInput('pinata-api-key');
+    const pinataSecretAPIKey = core.getInput('pinata-secret-api-key');
 
-  core.info(`Deploying to ${pinningService}`);
+    core.info(`Deploying to ${pinningService}`);
 
-  await deploy(
-    { 
-      remotePinners: pinningService,
-      pinata: {
-        apiKey: pinataAPIKey,
-        secretApiKey: pinataSecretAPIKey
-      } 
-    }
-  );
-} catch (error) {
-  core.setFailed(error.message);
-}
+    await deploy(
+      { 
+        remotePinners: pinningService,
+        pinata: {
+          apiKey: pinataAPIKey,
+          secretApiKey: pinataSecretAPIKey
+        } 
+      }
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
